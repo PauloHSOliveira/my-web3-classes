6599fef9f9fef506dfde07c43709c2fa575a9392
paulooliveira <paulooliveiradev@gmail.com>
2022-02-18 01:21:49 -0300

    created sanity client

---
+++ src/lib/sanityClient.ts
@@ -0,0 +1,10 @@
+import sanityClient from '@sanity/client'
+
+export const client = sanityClient({
+  projectId: 'mmqwi7y7',
+  dataset: 'production',
+  apiVersion: '2021-03-25',
+  token:
+    'skC9Z4C5XezqstsxpvVMkmC88u7uAGmKVUBkxXSSPgpU2DySH9ReimHvqM3mm55Pgf2vKMDSO0EWJPM3IvwEPQl8EY6R4XwxqTc1VHbjUNtaort2DleMOHWUGlCqpdUwId8WLE6mKcsfszFpYhNN3wqxTMc1Yc85GmGb7hCeOHfkak10FfAy',
+  useCdn: false
+})
