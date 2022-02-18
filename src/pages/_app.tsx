c6f1e66fdda576e60e35e07389256841385bfe2c
paulooliveira <paulooliveiradev@gmail.com>
2022-02-18 01:21:59 -0300

    added web3 provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ThirdwebWeb3Provider } from '@3rdweb/hooks'
+
+/**
+ * The chain ID 4 represents the Rinkeby network
+ * The `injected` connector is a web3 connection method used by Metamask
+ */
+const supportedChainsIds = [4]
+const connectors = {
+  injected: {}
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThirdwebWeb3Provider
+      supportedChainIds={supportedChainsIds}
+      connectors={connectors}
+    >
+      <Component {...pageProps} />
+    </ThirdwebWeb3Provider>
+  )
 }
 
 export default MyApp
