71e19bb3a7953039a24e49c5ac12a7b708235902
paulooliveira <paulooliveiradev@gmail.com>
2022-02-22 00:06:33 -0300

    create nft card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,21 +1,76 @@
 /* eslint-disable @next/next/no-img-element */
-// const style = {
-//   wrapper: `bg-[#303339] flex-auto w-[14rem] h-[22rem] my-10 mx-5 rounded-2xl overflow-hidden cursor-pointer`,
-//   imgContainer: `h-2/3 w-full overflow-hidden flex justify-center items-center`,
-//   nftImg: `w-full object-cover`,
-//   details: `p-3`,
-//   info: `flex justify-between text-[#e4e8eb] drop-shadow-xl`,
-//   infoLeft: `flex-0.6 flex-wrap`,
-//   collectionName: `font-semibold text-sm text-[#8a939b]`,
-//   assetName: `font-bold text-lg mt-2`,
-//   infoRight: `flex-0.4 text-right`,
-//   priceTag: `font-semibold text-sm text-[#8a939b]`,
-//   priceValue: `flex items-center text-xl font-bold mt-2`,
-//   ethLogo: `h-5 mr-2`,
-//   likes: `text-[#8a939b] font-bold flex items-center w-full justify-end mt-3`,
-//   likeIcon: `text-xl mr-2`
-// }
+import { useState, useEffect } from 'react'
+import { BiHeart } from 'react-icons/bi'
+import Router from 'next/router'
 
-export default function Card({ nftItem }) {
-  return <img src={nftItem.image} alt="" />
+const style = {
+  wrapper: `bg-[#303339] flex-auto w-[14rem] h-[22rem] my-10 mx-5 rounded-2xl overflow-hidden cursor-pointer`,
+  imgContainer: `h-2/3 w-full overflow-hidden flex justify-center items-center`,
+  nftImg: `w-full object-cover`,
+  details: `p-3`,
+  info: `flex justify-between text-[#e4e8eb] drop-shadow-xl`,
+  infoLeft: `flex-0.6 flex-wrap`,
+  collectionName: `font-semibold text-sm text-[#8a939b]`,
+  assetName: `font-bold text-lg mt-2`,
+  infoRight: `flex-0.4 text-right`,
+  priceTag: `font-semibold text-sm text-[#8a939b]`,
+  priceValue: `flex items-center text-xl font-bold mt-2`,
+  ethLogo: `h-5 mr-2`,
+  likes: `text-[#8a939b] font-bold flex items-center w-full justify-end mt-3`,
+  likeIcon: `text-xl mr-2`
+}
+
+export default function Card({ nftItem, title, listings }) {
+  const [isListed, setIsListed] = useState(false)
+  const [price, setPrice] = useState(0)
+
+  useEffect(() => {
+    const listing = listings.find((item) => item?.asset?.id === nftItem.id)
+    if (listing) {
+      setIsListed(true)
+      setPrice(listing.buyoutCurrencyValuePerToken.displayValue)
+    }
+  }, [listings, nftItem])
+
+  return (
+    <div
+      className={style.wrapper}
+      onClick={() => {
+        Router.push({
+          pathname: `/nfts/${nftItem.id}`,
+          query: { isListed }
+        })
+      }}
+    >
+      <div className={style.imgContainer}>
+        <img className={style.nftImg} src={nftItem.image} alt={nftItem.name} />
+      </div>
+      <div className={style.details}>
+        <div className={style.info}>
+          <div className={style.infoLeft}>
+            <div className={style.collectionName}>{title}</div>
+            <div className={style.assetName}>{nftItem.name}</div>
+          </div>
+          {isListed && (
+            <div className={style.infoRight}>
+              <div className={style.priceTag}>Price</div>
+              <div className={style.priceValue}>
+                <img
+                  className={style.ethLogo}
+                  src="https://storage.opensea.io/files/6f8e2979d428180222796ff4a33ab929.svg"
+                  alt="eth"
+                />
+                {price}
+              </div>
+            </div>
+          )}
+        </div>
+        <div className={style.likes}>
+          <div className={style.likeIcon}>
+            <BiHeart />
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
